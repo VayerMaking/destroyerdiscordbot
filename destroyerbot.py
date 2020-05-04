1635e1073168326537ecb376f50a7d4ba354f922
VayerM8king <martin.vayer@gmail.com>
2020-05-04 22:25:06 +0300

    1

---
+++ destroyerbot.py
@@ -0,0 +1,52 @@
+import discord
+from discord.ext import commands
+import time
+
+bot = commands.Bot(command_prefix='$')
+amount = 20
+is_purge_active = False
+#client = discord.Client()
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+
+
+@bot.event
+async def on_message(message):
+    channel = message.channel
+    print("asdf")
+    print(is_purge_active)
+    if is_purge_active == True:
+        time.sleep(amount)
+        await channel.purge(limit = None)
+    else:
+        await channel.send("Purge is not active")
+
+@bot.command(pass_context = True)
+async def set(ctx, a: int):
+    amount = a
+    print(amount)
+    await ctx.send(amount)
+
+@bot.command()
+async def add(ctx, a: int, b: int):
+    await ctx.send(a+b)
+
+@bot.command()
+async def purge_status():
+    print(is_purge_active)
+    return is_purge_active
+@bot.command()
+async def activate_purge():
+    is_purge_active = True
+    return is_purge_active
+@bot.command()
+async def deactivate_purge():
+    is_purge_active = False
+    return is_purge_active
+
+bot.run('NzA2ODc3MTExODMwNTc3MTY1.XrAqAQ.nk-r9bD_SfjQmETQ0dF61lSjmKY')
